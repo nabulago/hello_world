a18a216e748e7f6ec22ea919dd8d7e8200eab46d
nabulago <rollermax@gmail.com>
2024-09-13 01:18:03 +0530

    updated printing statement missing placeholders.

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,7 @@ fn primitive_examples(){
     let a_float: f64 = 1.0; // Regular annotation
     let an_integer = 5i32; // Suffix annotation
 
-    println!(logical, a_float, an_integer);
+    println!("{} {} {}",logical, a_float, an_integer);
 
 
 }
