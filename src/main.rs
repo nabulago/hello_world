b0c73f43caaa763587022cd6019144006f171f4e
nabulago <rollermax@gmail.com>
2024-09-13 01:15:40 +0530

    new function to test primitive and datatype

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+fn primitive_examples(){
+
+    // Variables can be type annotated.
+    let logical: bool = true;
+    let a_float: f64 = 1.0; // Regular annotation
+    let an_integer = 5i32; // Suffix annotation
+
+
+}
+
 fn main() {
     println!("Hello, world!");
+
+    primitive_examples()
+
 }
