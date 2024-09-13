e078bcc4e24016cd5a341d72c1c2184ee641eb6e
nabulago <rollermax@gmail.com>
2024-09-14 01:32:33 +0530

    added new var wuth data types

--- src/main.rs
+++ src/main.rs
@@ -5,8 +5,27 @@ fn primitive_examples(){
     let a_float: f64 = 1.0; // Regular annotation
     let an_integer = 5i32; // Suffix annotation
 
-    println!("{} {} {}",logical, a_float, an_integer);
+    // or a default will be used.
+    let default_float = 3.0;    // 'f64'
+    let default_int = 7;        // 'i32'
 
+    // A type can also be inferred from context
+    let mut inferred_type = 12; // Type i64 is inferred from another line.
+    inferred_type = 1234567891i64;
+
+    // A mutable variable'svalue can be changed.
+    let mut mutable = 12; // Mutable 'i32'
+    mutable = 21;
+
+    //Error! The type of a variable can't be changed.
+    mutable True;
+
+    // Variables can be overwritten with shadowing    
+    let mutable = True;
+
+    println!("{} {} {}",logical, a_float, an_integer);
+    println!("Default float{}", default_float);
+    println!("Default int{}", default_int);
 
 }
 
